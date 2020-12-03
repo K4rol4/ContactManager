14b6a5c4855c9305ebe2263a5f0d3aab32612e62
Karol Raciborski <karol.raciborski@gmail.com>
2020-12-03 17:24:48 +0100

    After adding recyclerview

--- app/src/main/java/com/example/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/contactmanager/MainActivity.java
@@ -1,51 +1,51 @@
 package com.example.contactmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.contactmanager.adapter.RecyclerViewAdapter;
 import com.example.contactmanager.data.DatabaseHandler;
 import com.example.contactmanager.model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private ArrayList<Contact> contactArrayList;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        contactArrayList = new ArrayList<>();
         DatabaseHandler db = new DatabaseHandler(MainActivity.this);
-
-        Log.d("Count", "onCreate: " + db.getCount());
-
-        //create contact object first
-        Contact jeremy = new Contact();
-        jeremy.setName("Jeremy");
-        jeremy.setPhoneNumber("985747374");
-
-        Contact json = new Contact();
-        json.setName("Jason");
-        json.setPhoneNumber("0877744");
-
-        //Get 1 contact
-//        Contact c = db.getContact(2);
-//        c.setName("NewJson");
-//        c.setPhoneNumber("2342");
-
-//        int updateRow = db.updateContact(c);
-//        Log.d("RowId", "onCreate: " + updateRow);
-
-        //db.addContact(json);
-
-        //delete a contact
-        //db.deleteContact(c);
 
         List<Contact> contactList = db.getAllContacts();
 
         for(Contact contact : contactList) {
-            Log.d("MainActivity", "onCreate: " + contact.getId());
+            Log.d("MainActivity", "onCreate: " + contact.getName());
+            contactArrayList.add(contact);
         }
+
+        //setup adapter
+        recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, contactArrayList);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+
     }
 }
